10c85831fd6c837978f7550f3f2d0abd28ba15a2
Genghui Zhu <23b902033@stu.hit.edu.cn>
2026-01-22 11:12:36 +0000

    add fixed normal mode to KSTemperature function

--- src/adjoint/DAFunction/DAFunctionTemperatureKS.C
+++ src/adjoint/DAFunction/DAFunctionTemperatureKS.C
@@ -32,6 +32,12 @@ DAFunctionTemperatureKS::DAFunctionTemperatureKS(
 
     functionDict_.readEntry<scalar>("coeffKS", coeffKS_);
     isGrad_ = functionDict_.lookupOrDefault<bool>("isGrad", false);
+    normalMode_ = functionDict_.lookupOrDefault<word>("normalMode", "auto");
+
+    if (normalMode_ == "fixed")
+    {
+        functionDict_.readEntry<scalar>("normalValue", normalValue_);
+    }
 }
 
 /// calculate the value of objective function
@@ -48,16 +54,26 @@ scalar DAFunctionTemperatureKS::calcFunction()
     const objectRegistry& db = mesh_.thisDb();
     const volScalarField& T = db.lookupObject<volScalarField>("T");
 
-    
     volScalarField tmpField = T;
     if (isGrad_)
     {
         tmpField = mag(fvc::grad(T)) * dimensionedScalar("unitLength", dimLength, 1.0);
     }
 
-    scalar tmpMaxValue = gMax(tmpField);
-    scalar maxValue;
-    assignValueCheckAD(maxValue, tmpMaxValue);
+    scalar maxValue = 0;
+    if (normalMode_ == "auto")
+    {
+        scalar tmpMaxValue = gMax(tmpField);
+        assignValueCheckAD(maxValue, tmpMaxValue);
+    }
+    else if (normalMode_ == "fixed")
+    {
+        assignValueCheckAD(maxValue, normalValue_);
+    }
+    else
+    {
+        FatalErrorIn(" ") << "KS function normalMode must be auto or fixed! " << abort(FatalError);
+    }
 
     scalar objValTmp = 0.0;
     forAll(cellSources_, idxI)
